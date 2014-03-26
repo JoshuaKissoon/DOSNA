a0c81489d3321eb9e71eac6b051a0d1756be69ba
Joshua Kissoon <JoshuaKissoon@gmail.com>
2014-03-26 12:51:53 +0530

    Bootstrap

---
+++ src/dosna/bootstrap/Bootstrap.java
@@ -0,0 +1,21 @@
+package dosna.bootstrap;
+
+/**
+ * Here we load the bootstrap node and keep it running
+ *
+ * @author Joshua Kissoon
+ * @since 20140326
+ */
+public class Bootstrap
+{
+
+    public Bootstrap()
+    {
+        
+    }
+
+    public static void main(String[] args)
+    {
+        
+    }
+}
